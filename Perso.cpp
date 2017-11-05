386bc3e13ad358870bde364ec73dc9c4788ed917
Thunderkilll <khaled.guesmi@esprit.tn>
2017-11-05 20:38:05 +0100

    i changed in the Perso.h corrected some mistakes of confusion between c and c++ in .cpp

--- Perso.cpp
+++ Perso.cpp
@@ -5,15 +5,23 @@ using namespace std;
 
 
 
-void afficher() {
+Perso::afficher() {
 
   /* code */
   return "position x :"+getX()+"position y :"+getY()+"Numero de mouvement :"+getUnit_Move();
 }
 
-Perso (SDL_Rect x , SDL_Rect y , int unit_move){
+Perso::Perso(SDL_Rect x , SDL_Rect y , int unit_move){
 this.x=x;
 this.y=y;
 this.unit_move=unit_move;
 
 }
+/*
+void ajoutPersonnage(Perso p)
+{
+p.setX();
+p.setY();
+p.setUnit_Move();
+
+}*/
--- Perso.h
+++ Perso.h
@@ -18,7 +18,7 @@ int unit_move;
 SDL_Rect getX(){return x;}
 SDL_Rect getY(){return y;}
 int getUnit_Move(){return unit_move;}
-//int getIdentifiant(){return identifiant;} 
+//int getIdentifiant(){return identifiant;}
 void setX(SDL_Rect x){this->getX()=x;}
 void setY(SDL_Rect y){this->getY()=y;}
 void setUnit_Move(int unit_move){this->unit_move=unit_move;}
@@ -26,7 +26,7 @@ void setUnit_Move(int unit_move){this->unit_move=unit_move;}
  Perso(){unit_move=0;}
  Perso(SDL_Rect x , SDL_Rect y , int unit_move);
  void afficher();
- 
+ //void ajoutPersonnage(Perso p);
 
 
 };
